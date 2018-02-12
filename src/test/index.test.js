32dc7d1d7f07b89601fb50933c666245ce9b8ddb
randy-darsh <rspringer47@gmail.com>
2018-02-12 12:04:39 -0700

    started test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,9 +1,19 @@
 import { expect } from "chai"
 import sinon from "sinon"
+const chai = require('chai')
+const should = chai.should()
+const chaiHTTP = require('chai-http')
+const server = require('../index.js')
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("GET /api/v1/top_word", () => {
+    it("they should see the top word along with it's count", () => {
+      return chai.request(server)
+      .get("/api/v1/top_word")
+      .then(response => {
+        response.should.have.status(200)
+        response.should.be.json
+      })
     })
   })
 })
