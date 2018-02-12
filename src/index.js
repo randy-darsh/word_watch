f736e03c0c8c49e1259c46052c4aabd8c8a54f60
randy-darsh <rspringer47@gmail.com>
2018-02-12 12:04:24 -0700

    almost got story 2 but i broke it at the end when i was trying to dynamicall add words.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word")
+  .then(response => { return response.json() })
+  .then(data => {
+    $('.word-count').prepend(
+      "<div class='row'>" +
+      `<div class="row-name">Word: ${Object.keys(data.word)[0]}</div>` +
+      `<div class="row-number">Count: ${Object.values(data.word)[0]}</div>` +
+      "</div>"
+    )
+  })
+})
+
+$(document).ready(() => {
+  $('button').on('click', function(event) {
+    var text = $('textarea').val()
+    var wordArray = text.toLowerCase().split(" ")
+    var counts = {}
+    for (var i = 0, j = wordArray.length; i < j; i++) {
+      counts[wordArray[i]] = (counts[wordArray[i]] || 0) + 1
+    }
+    $('.word-count').append(Object.keys(counts).forEach((word) => {
+      "<div class='row'>" +
+      `<div class="row-name">Word: <font size="${counts[word]}em">${word}</font></div>` +
+      "</div>"
+      // debugger
+      })
+    )
+  })
+})
+
+$(document).ready(() => {
+  function createNewWord(word) {
+    var newWord = { "word": { "value": "${word}" } }
+    fetch("https://wordwatch-api.herokuapp.com/api/v1/words", {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newWord)
+    })
+    .then(response => {
+      return response.json()
+    })
+  }
 })
